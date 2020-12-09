83cd23a343b5394782486125751f82cde5c6eda5
Hectoliters <48892625+SOULTALE@users.noreply.github.com>
2020-12-09 15:35:13 -0700

    Updated Loop.js
    My first collab ever with STR1KE

--- mods/Loop.js
+++ mods/Loop.js
@@ -1,16 +1,20 @@
+//thank you strike for helping me fix the variables resetting ~Hectoliters
+//No problem :D ~ STR1KE
+var i = 0;
+var times = 1;
 module.exports = {
     // Set this to the name of the mod. This is what will be shown inside of Discord Bot Studio.
     // THIS FILE NAME MUST BE THIS VALUE WITH SPACES REMOVED
     name: "Loop",
 
     // Place the author of the mod here. This is an array so you can add other authors by writing ["Great Plains Modding", "New User"]
-    author: ["Hectoliters#7743"],
+    author: ["Hectoliters#7743", "STR1KE#6969"],
 
     // Place the version of the mod here.
-    version: "0.1.0",
+    version: "1.0.0",
 
     // Whenever you make a change, please place the changelog here with your name. Created Send Message ~ Great Plains Modding\n
-    changelog: "Created Loop ~ Hectoliters",
+    changelog: "Updated Loop ~ Hectoliters",
 
     // Set this to true if this will be an event.
     isEvent: false,
@@ -29,6 +33,17 @@ module.exports = {
     // Place your html to show inside of Discord Bot Studio when they select your mod.
     html: function(data) {
         return `
+        <div class="form-group">
+        <label>Loop Mode *</label>
+        <select class="form-control" name="LoopMode">
+            <option value="Infinite" selected>Infinite</option>
+            <option value="Finite">Finite</option>
+        </select>
+    </div>
+    <div class="form-group">
+    <label>Ammount of times to loop it (Optional) </label>
+    <textarea class="form-control field" name="ammount" rows="1" ></textarea>
+</div>
             <div class="form-group">
                 <label>Ammount of nodes to loop (Before this one) * </label>
                 <textarea class="form-control needed-field" name="Nodes" rows="1" ></textarea>
@@ -49,10 +64,41 @@ module.exports = {
     mod: function (DBS, message, action, args, command, index) {
         // counting each second as 1.1 because otherwise it stops itself
         var delay = action.timedelay * 1100;
-
+         i = action.ammount;
+switch (action.loopmode) {
+    case "Infinite" :
+        //Travel Back
 setTimeout(function() {
     DBS.callNextAction(command, message, args, index - action.nodes);
     console.log("Traveled" + action.nodes + "nodes back")
 }, delay);
-    }
+
+
+
+break;
+
+
+
+case "Finite":
+    if (times > -2){
+        times = times + 1;
+      } else {
+       times = 1;
+      }
+        i = i - times + 2;
+    if (i > 0) {
+        setTimeout(function() {
+           //Travel Back
+              DBS.callNextAction(command, message, args, index - action.nodes);
+              console.log("Traveled" + action.nodes + "nodes back")
+          }, delay);
+     } else {
+          //Stop Loop
+            console.log("Stopped Looping")
+         DBS.callNextAction(command, message, args, index + 1);
+     }
+break;
+}
+
+}
 };
