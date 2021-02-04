a9efb618225f56ad30bc7f88979770094f390b85
Great Plains Modding <46289624+greatplainsmodding@users.noreply.github.com>
2021-02-04 15:38:26 -0600

    Update CommandCooldown.js

--- mods/CommandCooldown.js
+++ mods/CommandCooldown.js
@@ -50,7 +50,7 @@ module.exports = {
     },
 
     // When the bot is first started this code will be ran.
-    init: function () {
+    init: function (DBS) {
         console.log("Loaded Cooldown Mod");
         DBS.CommandCooldown = {}
     },
