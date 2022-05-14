c303d0074f0f90d743c8674ba5a9be9f3d16712e
Playboy <55946112+PlayboyPrime@users.noreply.github.com>
2022-05-14 20:56:10 +0200

    Update PingMessage.js

--- mods/PingMessage.js
+++ mods/PingMessage.js
@@ -46,6 +46,7 @@ module.exports = {
 
     // When the bot is first started, this code will be ran.
     init: function(DBS) {
+        if (!DBS.BetterMods) return console.log(`\x1b[36m [${this.name}.JS] \x1b[0m\x1b[31mBetterMods.js is not loaded. BetterMods.js is required to use this mod. \x1b[0m`);
         console.log("Loaded ping message mod");
     },
 
