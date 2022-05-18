fd59a6efc1b86f250242040d2c7776ef29ee0694
Playboy <55946112+PlayboyPrime@users.noreply.github.com>
2022-05-18 23:56:35 +0200

    Update Leaderboard.js

--- mods/Leaderboard.js
+++ mods/Leaderboard.js
@@ -41,6 +41,7 @@ module.exports = {
         `;
     },
     init: function (DBS) {
+        if (!DBS.BetterMods) return console.log(`\x1b[36m [${this.name}.JS] \x1b[0m\x1b[31mBetterMods.js is not loaded. BetterMods.js is required to use this mod. \x1b[0m`);
         console.log("Loaded Leaderboard");
     },
     mod: async function (DBS, message, action, args, command, index) {
